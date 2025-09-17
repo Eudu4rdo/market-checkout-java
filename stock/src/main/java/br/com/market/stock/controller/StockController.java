7f21c5f896ec92c9094bd5cfec572d4adb16654e
Eduardo Garcia <eg47202@gmail.com>
2025-09-17 20:07:33 -0300

    feat: incluindo rotas para as features do commit anterior
    Alem das rotas para os commits, foi criada o arquivo para criar a imagem docker e foi incluso ao docker-compose
    Foi criada tambem o DTO para quantity, com a finalidade de manter as boas praticas.

--- stock/src/main/java/br/com/market/stock/controller/StockController.java
+++ stock/src/main/java/br/com/market/stock/controller/StockController.java
@@ -1,11 +1,11 @@
 package br.com.market.stock.controller;
 
 import br.com.market.stock.dto.ProductDTO;
+import br.com.market.stock.dto.QuantityDTO;
+import br.com.market.stock.model.Product;
 import br.com.market.stock.service.StockService;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -23,5 +23,29 @@ public class StockController {
         return ResponseEntity.ok(stockService.getAllProducts());
     }
 
+    @PostMapping("/product/add")
+    public ResponseEntity<ProductDTO> addProduct(@RequestBody Product product){
+        return ResponseEntity.ok(stockService.addProduct(product));
+    }
 
+    @DeleteMapping("/product/{id}/delete")
+    public ResponseEntity<ProductDTO> deleteProduct(@PathVariable Long id){
+        return ResponseEntity.ok(stockService.deleteProduct(id));
+    }
+
+    @PostMapping("/product/{id}/edit")
+    public ResponseEntity<ProductDTO> updateProduct(@PathVariable Long id, @RequestBody Product product){
+        System.out.println(product);
+        return ResponseEntity.ok(stockService.updateProduct(id, product));
+    }
+
+    @PostMapping("/product/{id}/increase")
+    public ResponseEntity<ProductDTO> increaseQuantity(@PathVariable Long id, @RequestBody QuantityDTO dto){
+        return ResponseEntity.ok(stockService.increaseQuantity(id, dto.getQuantity()));
+    }
+
+    @PostMapping("/product/{id}/decrease")
+    public ResponseEntity<ProductDTO> decreaseQuantity(@PathVariable Long id, @RequestBody QuantityDTO dto){
+        return ResponseEntity.ok(stockService.decreaseQuantity(id, dto.getQuantity()));
+    }
 }
---
+++ stock/src/main/java/br/com/market/stock/dto/QuantityDTO.java
@@ -0,0 +1,12 @@
+package br.com.market.stock.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@AllArgsConstructor
+@NoArgsConstructor
+public class QuantityDTO {
+    private int quantity;
+}
